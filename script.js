07ba3d4a35fc1e1f02ef12c04a0176be3819ae66
fidellim <lim.fidel@gmail.com>
2021-10-01 01:03:40 +0400

    add slider for hero

---
+++ script.js
@@ -0,0 +1,81 @@
+// const slides = document.querySelectorAll(".slide");
+const slide = document.querySelector(".slide");
+const heroHeading = document.querySelector(".hero-heading-green");
+const heroGold = document.querySelector(".hero-gold-bg");
+const heroGreen = document.querySelector(".hero-green-bg");
+const sliderContainer = document.querySelector(".slider-container");
+
+let activeSlide = 0;
+
+const heroHeadingTexts = ["ecommerce", "retail", "groceries", "food"];
+
+const slideImages = [
+	"./images/hero/hero_1.png",
+	"./images/hero/hero_2.png",
+	"./images/hero/hero_3.png",
+	"./images/hero/hero_4.png",
+];
+
+const setActiveSlide = () => {
+	setTimeout(() => {
+		slide.classList.remove("active");
+		heroHeading.classList.remove("active");
+	}, 0);
+
+	setTimeout(() => {
+		slide.src = slideImages[activeSlide];
+		slide.style.transition = "1s ease";
+		slide.classList.add("active");
+		heroHeading.innerHTML = heroHeadingTexts[activeSlide];
+		heroHeading.style.transition = "1s ease";
+		heroHeading.classList.add("active");
+		createHeroGold();
+		createHeroGreen();
+	}, 1000);
+};
+
+const moveSlide = () => {
+	if (activeSlide === slideImages.length - 1) {
+		activeSlide = 0;
+	} else {
+		activeSlide++;
+	}
+
+	setActiveSlide();
+};
+
+const createHeroGold = () => {
+	const tag = document.createElement("img");
+	tag.src = "./images/gold_bg.png";
+	tag.classList.add("hero-gold-bg");
+	sliderContainer.appendChild(tag);
+
+	removeHeroBg(tag);
+};
+
+const createHeroGreen = () => {
+	const tag = document.createElement("img");
+	tag.src = "./images/green_bg.png";
+	tag.classList.add("hero-green-bg");
+	sliderContainer.appendChild(tag);
+
+	removeHeroBg(tag);
+};
+
+const removeHeroBg = (el) => {
+	setTimeout(() => {
+		el.remove();
+	}, 2000);
+};
+
+// add opacity 1 to hero section data
+slide.classList.add("active");
+heroHeading.classList.add("active");
+// add transition after 1 sec after rendering in the beginning
+setTimeout(() => {
+	slide.style.transition = "1s ease";
+	heroHeading.style.transition = "1s ease";
+}, 1000);
+
+// change hero section data every 3.5 secs
+setInterval(moveSlide, 3500);
